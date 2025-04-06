c00c93b374fd2abafdbe9050ffb0567e7d91c489
JOCKS <josaji2026@cs.sjcetpalai.ac.in>
2025-04-06 22:58:45 +0530

    USER INTERFACE SET

--- safecircle-backend/src/models/User.js
+++ safecircle-backend/src/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true,unique: true },
     address: { type: String, required: true },
-    aadhar: { type: String, required: true },
+    aadhar: { type: String, required: true, unique: true },
     phone: {
       type: String,
       required: true,
